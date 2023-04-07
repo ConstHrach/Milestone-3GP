7d315b5d9e5fe05a80d58531d697633405b7495c
ConstHrach <hrachya62799@gmail.com>
2023-04-06 18:14:32 -0700

    file directory

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import posts from "../components/posts";
+import comments from "../components/comments";
+
+
+function Home() {
+  return (
+    <div>
+      <posts />
+      <comments />
+    </div>
+  )
+}
+
+export default Home
---
+++ frontend/src/pages/SignUp.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function SignUp() {
+  return (
+    <div className='wrapper'>SignUp</div>
+  )
+}
+
+export default SignUp
